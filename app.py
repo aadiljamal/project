435f93d48a33cb0b938a044a36cc5b0c87b991e2
aadiljamal <mraadil.jamal@gmail.com>
2021-07-24 21:48:01 +0530

    testing i/o datastore

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from google.cloud import firestore
 db = firestore.Client.from_service_account_json("mindreader-firestore-key.json")
 
 # Create a reference to the image data.
-doc_ref = db.collection("MITR").document("mitr-happy-out-umbrella")
+doc_ref = db.collection("MITR").document("mitr-happy-without-umbrella")
 
 
 
@@ -41,6 +41,8 @@ canvas_result = st_canvas(
 
 st.button("save drawing")
 image_data = canvas_result.image_data
+#ndarray to array conversion
+image_data_for-firestore = image_data.ravel()
 jsonformat = canvas_result.json_data
 
 if canvas_result.json_data is not None:
@@ -48,10 +50,11 @@ if canvas_result.json_data is not None:
 
 # Then get the data at that reference.
 doc = doc_ref.set({
-    "drawing":image_data
+    "drawing":image_data_for-firestore 
     })
 
 
 # Let's see what we got!
 st.write("The id is: ", doc.id)
-st.write("The Drawing data are: ", doc.to_dict())
+
+st.write("The Drawing data are: ", np.ndarray((1,4), buffer = doc.to_dict(), offset = np.int_().itemsize,           dtype = int))
