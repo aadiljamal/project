c6198457f9f2e40cc9f67d7a2fe4f6df13e645be
aadiljamal <mraadil.jamal@gmail.com>
2021-07-26 13:23:57 +0530

    new key added

--- app.py
+++ app.py
@@ -13,42 +13,6 @@ from struct import unpack
 import json
 from google.oauth2 import service_account
 
-
-"""def unpack_drawing(file_handle):
-    key_id, = unpack('Q', file_handle.read(8))
-    country_code, = unpack('2s', file_handle.read(2))
-    recognized, = unpack('b', file_handle.read(1))
-    timestamp, = unpack('I', file_handle.read(4))
-    n_strokes, = unpack('H', file_handle.read(2))
-    image = []
-    for i in range(n_strokes):
-        n_points, = unpack('H', file_handle.read(2))
-        fmt = str(n_points) + 'B'
-        x = unpack(fmt, file_handle.read(n_points))
-        y = unpack(fmt, file_handle.read(n_points))
-        image.append((x, y))
-
-    return {
-        'key_id': key_id,
-        'country_code': country_code,
-        'recognized': recognized,
-        'timestamp': timestamp,
-        'image': image
-    }
-
-
-def unpack_drawings(filename):
-    with open(filename, 'rb') as f:
-        while True:
-            try:
-                yield unpack_drawing(f)
-            except struct.error:
-                break
-
-
-"""
-
-
 # Authenticate to Firestore with the JSON account key.
 #db = firestore.Client.from_service_account_json("mindreader-firestore-key.json")
 
