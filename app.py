c87139baf7d834927e0e91e312396cc7b01eefdc
aadiljamal <mraadil.jamal@gmail.com>
2021-11-21 00:41:39 +0530

    new model implemented

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #TF is the main ML/AI library and keras is its high level api
 from numpy.core.fromnumeric import size
-from keras.preprocessing.image import load_img
+#from keras.preprocessing.image import load_img
 #from keras.preprocessing.image import save_img
 #from keras.preprocessing.image import img_to_array
 
@@ -136,7 +136,7 @@ if  (canvas_result.image_data is not  None) and realtime_update == True  :
     # ... do stuff with dirpath        
     X = (Image.fromarray((canvas_result.image_data).astype(np.uint8))).save(f'{dirpath}/{filename}.png') 
     #os.system(cmd)
-    test_image = X  #load_img((f'{dirpath}/{filename}.png'), target_size = (224, 224)) 
+    test_image = X #load_img((f'{dirpath}/{filename}.png'), target_size = (224, 224)) 
     test_image = img_to_array(test_image)
     #test_image = np.expand_dims(test_image, axis = 0)
     save_img(f'{dirpath}/{filename}con.png',test_image)
